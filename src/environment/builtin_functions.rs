141a95d60f19c45b3c6f79c9ed3fbb7a593b7932
APIBox <LinkerVB@outlook.com>
2025-05-14 21:34:05 +0800

    🥳 添加两个内置函数
    1. clear函数: 通过特定ascii清除终端
    2. now函数: 获取从Unix元年到现在的秒数

--- src/environment/builtin_functions.rs
+++ src/environment/builtin_functions.rs
@@ -1,19 +1,24 @@
 use std::io;
 use std::io::Write;
+use std::time::{SystemTime, UNIX_EPOCH};
+
+use bigdecimal::{BigDecimal, FromPrimitive};
+
 use crate::environment::environment::Environment;
+use crate::object::ant_double::AntDouble;
 use crate::object::ant_string::AntString;
 use crate::object::object::IAntObject;
 
 pub fn builtin_print(arg_env: &mut Environment) -> Option<Box<dyn IAntObject>> {
-    let value = arg_env.get("value");
-    match value {
-        None => {
-            panic!("what the fuck? arg_env: {}", arg_env.to_string())
-        }
-        Some(it) => {
-            println!("{}", it.inspect());
-        }
-    }
+    let value = arg_env.get("value").expect(
+        &format!("cannot find \"value\". arg_env: {}", arg_env.to_string())
+    );
+
+    let end = arg_env.get("end").expect(
+        &format!("cannot find \"end\". arg_env: {}", arg_env.to_string())
+    );
+
+    print!("{}{}", value.inspect(), end.inspect());
 
     None
 }
@@ -40,4 +45,28 @@ pub fn builtin_input(arg_env: &mut Environment) -> Option<Box<dyn IAntObject>> {
             Some(AntString::new_with_native_value(Box::new(input)))
         }
     }
+}
+
+pub fn builtin_clear(_arg_env: &mut Environment) -> Option<Box<dyn IAntObject>> {
+    print!("\x1b[2J");
+    print!("\x1b[H");
+
+    None
+}
+
+pub fn builtin_now(_arg_env: &mut Environment) -> Option<Box<dyn IAntObject>> {
+    Some(
+        AntDouble::new_with_native_value(Box::new(BigDecimal::from_f64(
+            SystemTime::now()
+                .duration_since(UNIX_EPOCH)
+                .map(|duration| {
+                    duration
+                        .as_secs() as f64
+                        + duration
+                            .subsec_nanos() as f64
+                            / 1_000_000_000.0
+                })
+                .unwrap_or(-1.0)
+        ).unwrap()))
+    )
 }
