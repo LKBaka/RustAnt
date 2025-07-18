7b4119c6d59752586c851dca1f8430a6430c748e
APIBox <LinkerVB@outlook.com>
2025-07-18 18:18:56 +0800

    🛠️ 修复了先前的 Bug

--- src/parser/parse_functions/parse_call_expression.rs
+++ src/parser/parse_functions/parse_call_expression.rs
@@ -1,18 +1,16 @@
+use crate::token::token_type::TokenType;
+
 use crate::ast::ast::Expression;
 use crate::ast::expressions::call_expression::create_call_expression;
 use crate::parser::parser::Parser;
-use crate::token::token_type::TokenType::RParen;
+
 
 pub fn parse_call_expression(parser: &mut Parser, left: Box<dyn Expression>) -> Option<Box<dyn Expression>> {
     let token = parser.cur_token.clone();
 
-    let func = left;
+    let args = parser.parse_expression_list(TokenType::RParen);
 
-    // 离开表达式
-    parser.next_token();
-
-    // 解析参数
-    let args = parser.parse_expression_list(RParen);
-
-    Some(Box::new(create_call_expression(token, func, args)))
+    Some(Box::new(
+        create_call_expression(token, left, args)
+    ))
 }
