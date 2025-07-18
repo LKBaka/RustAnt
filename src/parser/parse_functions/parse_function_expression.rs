8ef72fb1f3cbc45322cc2a1929a4498dcfe6c1a0
APIBox <LinkerVB@outlook.com>
2025-07-18 18:23:20 +0800

    🔨 健壮性++ 现在在缺少函数体时会正常报错了

--- src/parser/parse_functions/parse_function_expression.rs
+++ src/parser/parse_functions/parse_function_expression.rs
@@ -1,50 +1,63 @@
 use std::any::Any;
-use crate::ast::ast::Expression;
-use crate::ast::expressions::function_expression::create_function_expression;
+
 use crate::ast::statements::block_statement::BlockStatement;
 use crate::parser::parse_functions::parse_block_statement::parse_block_statement;
+use crate::token::token_type::TokenType;
+
+use crate::ast::ast::Expression;
+use crate::ast::expressions::function_expression::create_function_expression;
 use crate::parser::parser::Parser;
-use crate::token::token_type::TokenType::{Ident, LParen, RParen};
+
 
 pub fn parse_function_expression(parser: &mut Parser) -> Option<Box<dyn Expression>> {
-    let mut name: Option<String> = None;
     let token = parser.cur_token.clone();
-    
-    // 解析参数列表
-    parser.next_token(); // 离开 func 关键字
 
-    // 判断是否存在标识符（名字）
-    if parser.cur_token_is(Ident) {
-        name = Some(parser.cur_token.value.clone());
-        parser.next_token(); // 离开标识符
-    }
-
-    if !parser.expect_cur(LParen) {
-        return None;
-    }
-    
-    parser.next_token(); // 离开左括号
-
-    // 解析参数列表
-    let parameters = parser.parse_expression_list(RParen);
-    
-    if !parser.expect_cur(RParen) {
-        return None;
-    }
-    
-    parser.next_token(); // 离开右括号
-    
-    // 解析函数体
-    let body = parse_block_statement(parser);
-    if body.is_none() {
+    if !parser.expect_peek(TokenType::Ident) {
         return None;
     }
 
-    let node = body.unwrap() as Box<dyn Any>;
+    parser.next_token(); // 前进到标识符
 
-    if let Some(body) = node.downcast_ref::<BlockStatement>() {
-        return Some(Box::new(create_function_expression(token, name, parameters, body.to_owned())));
+    let name = Some(parser.cur_token.value.clone());
+
+    if !parser.expect_peek(TokenType::LParen) {
+        return None;
     }
 
-    None
+    parser.next_token(); // 前进到左括号
+
+    let params = parser.parse_expression_list(TokenType::RParen);
+    
+    parser.next_token(); // 离开右括号 (正常应前进到左大括号)
+
+    let block = parse_block_statement(parser);
+
+    match block { 
+        Some(block) => {
+            let converted_block = 
+                (block.as_ref() as &dyn Any)
+                    .downcast_ref::<BlockStatement>()
+                    .expect(&format!(
+                        "expected block statement, got {:?}, ast to string: {}", 
+                        block.type_id(), 
+                        block.to_string()
+                    ))
+                    .clone();
+
+            Some(Box::new(create_function_expression(
+                token, 
+                name,
+                params, 
+                converted_block
+            )))
+        }
+        None => {
+            parser.errors.push(
+                format!(
+                    "missing function body. at file <{}>, line {}",
+                    parser.cur_token.file, parser.cur_token.line
+                )
+            ); None
+        }
+    }
 }
