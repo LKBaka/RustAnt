dbf6e8f2ac03812819ea42d7c3e8657dc966ccae
APIBox <LinkerVB@outlook.com>
2025-07-18 16:48:27 +0800

    🧐 支持标识符只读

--- src/environment/environment.rs
+++ src/environment/environment.rs
@@ -1,5 +1,7 @@
+use std::cell::RefCell;
 use std::cmp::PartialEq;
 use std::ops::Deref;
+use std::rc::Rc;
 
 use crate::environment::data::Data;
 use crate::map;
@@ -11,11 +13,11 @@ use crate::object::utils::{create_error, create_error_with_name};
 pub struct Environment {
     pub map: Map<String, Data>,
     pub func_map: Map<String, Vec<Data>>,
-    pub outer: Option<Box<Environment>>,
+    pub outer: Option<Rc<RefCell<Environment>>>,
 }
 
 impl Environment {
-    pub fn new_with_outer(outer: Box<Environment>) -> Environment {
+    pub fn new_with_outer(outer: Rc<RefCell<Environment>>) -> Environment {
         Environment {
             map: map!(),
             func_map: map!(),
@@ -46,20 +48,20 @@ impl Environment {
         self.func_map.pairs.retain(|pair| !pair.value.is_empty());
     }
 
-    pub fn remove_obj(&mut self, value: Object) {
-        self.map.pairs.retain(|pair| !(pair.value.data == value.clone()));
+    pub fn remove_obj(&mut self, value: &Object) {
+        self.map.pairs.retain(|pair| !((&pair.value.data) == value));
 
         for func_pair in &mut self.func_map.pairs {
-            func_pair.value.retain(|v| !(v.data == value.clone()));
+            func_pair.value.retain(|v| !(&v.data == value));
         }
 
         self.func_map.pairs.retain(|pair| !pair.value.is_empty());
     }
 
     pub fn create(&mut self, key: &str, value: Data) -> Option<Object> {
-        if self.map.contains_key(key.to_string()) {
+        if self.map.contains_key(&key.to_string()) {
             return Some(
-                create_error_with_name("NameError", format!("variable \"{}\" already exists", key))
+                create_error_with_name("NameError", format!("identifier '{}' already exists", key))
             );
         }
 
@@ -68,26 +70,44 @@ impl Environment {
             return None;
         }
 
-        self.func_map.add(key.to_string(), vec![value]);
+        if self.func_map.contains_key(&key.to_string()) {
+            let mut data_array = self.func_map.get(&key.to_string()).unwrap().clone();
+            data_array.push(value);
+            
+            self.func_map.set(key.to_string(), data_array);
+        } else {
+            self.func_map.add(key.to_string(), vec![value]);
+        }
 
         None
     }
 
     pub fn set(&mut self, key: &str, value: Data) -> Option<Object> {
-        if self.map.contains_key(key.to_string()) {
+        if self.map.contains_key(&key.to_string()) {
+            if self.map.get(&key.to_string()).unwrap().info.readonly {
+                return Some(create_error_with_name(
+                    "ReadOnlyError", format!("'{}' is readonly", key)
+                ))
+            }
+
             self.map.set(key.to_string(), value);
             return None;
         }
 
         Some(
-            create_error(format!("cannot find variable \"{}\"", key))
+            create_error(format!("cannot find variable '{}'", key))
         )
     }
 
     pub fn set_value(&mut self, key: &str, value: Object) -> Option<Object> {
-        if self.map.contains_key(key.to_string()) {
+        if self.map.contains_key(&key.to_string()) {
             let data = self.get_data(key);
-            if let Some(mut it) = data {
+
+            if let Some(it) = &data && it.info.readonly {
+                return Some(create_error_with_name(
+                    "ReadOnlyError", format!("'{}' is readonly", key)
+                ))
+            } else if let Some(mut it) = data {
                 it.data = value;
 
                 self.map.set(key.to_string(), it);
@@ -97,72 +117,72 @@ impl Environment {
         }
 
         Some(
-            create_error(format!("cannot find variable \"{}\"", key))
+            create_error(format!("cannot find variable '{}'", key))
         )
     }
 
-    pub fn get_data(&mut self, key: &str) -> Option<Data> {
-        if self.map.contains_key(key.to_string()) {
-            return Option::from(self.map.get(key.to_string()).clone())
+    pub fn get_data(&self, key: &str) -> Option<Data> {
+        if self.map.contains_key(&key.to_string()) {
+            return self.map.get(&key.to_string())
         }
 
-        if self.func_map.contains_key(key.to_string()) {
-            return Option::from(self.func_map.get(key.to_string()).unwrap()[0].clone())
+        if self.func_map.contains_key(&key.to_string()) {
+            return Option::from(self.func_map.get(&key.to_string()).unwrap()[0].clone())
         }
 
-        if let Some(mut outer) = self.outer.clone() {
-            return outer.get_data(key)
+        if let Some(outer) = &self.outer {
+            return outer.borrow().get_data(key)
         }
 
         None
     }
 
-    pub fn get_values(&mut self, key: &str) -> Option<Vec<Object>> {
-        if self.map.contains_key(key.to_string()) {
-            return Some(vec![self.map.get(key.to_string()).unwrap().data.clone()])
+    pub fn get_values(&self, key: &str) -> Option<Vec<Object>> {
+        if self.map.contains_key(&key.to_string()) {
+            return Some(vec![self.map.get(&key.to_string()).unwrap().data.clone()])
         }
 
-        if self.func_map.contains_key(key.to_string()) {
+        if self.func_map.contains_key(&key.to_string()) {
             let mut values = vec![];
-            let data_array = self.func_map.get(key.to_string()).unwrap();
+            let data_array = self.func_map.get(&key.to_string()).unwrap();
 
             for data in data_array {
-                values.push(data.data)
+                values.push(data.data.clone());
             }
 
             return Some(values)
         }
 
-        if let Some(mut outer) = self.outer.clone() {
-            return outer.get_values(key)
+        if let Some(outer) = &self.outer {
+            return outer.borrow().get_values(key)
         }
 
         None
     }
 
-    pub fn get(&mut self, key: &str) -> Option<Object> {
-        if self.map.contains_key(key.to_string()) {
-            return Some(self.map.get(key.to_string()).unwrap().data.clone())
+    pub fn get(&self, key: &str) -> Option<Object> {
+        if self.map.contains_key(&key.to_string()) {
+            return Some(self.map.get(&key.to_string()).unwrap().data.clone())
         }
 
-        if self.func_map.contains_key(key.to_string()) {
-            return Some(self.func_map.get(key.to_string()).unwrap()[0].data.clone())
+        if self.func_map.contains_key(&key.to_string()) {
+            return Some(self.func_map.get(&key.to_string()).unwrap()[0].data.clone())
         }
 
-        if let Some(mut outer) = self.outer.clone() {
-            return outer.get(key)
+        if let Some(outer) = &self.outer {
+            return outer.borrow().get(key)
         }
 
 
         None
     }
 
-    pub fn in_place_fusion(&mut self, other: Environment) {
+    pub fn in_place_fusion(&mut self, other: &Environment) {
         for pair in &other.map.pairs {
-            if self.map.contains_key(pair.key.clone()) {
-                self.set(pair.key.deref(), pair.value.clone());
+            if self.map.contains_key(&pair.key) {
+                self.set(&pair.key, pair.value.clone());
             } else {
-                self.create(pair.key.deref(), pair.value.clone());
+                self.create(&pair.key, pair.value.clone());
             }
         }
     }
@@ -171,10 +191,10 @@ impl Environment {
         let mut env = self.clone();
 
         for pair in &other.map.pairs {
-            if env.map.contains_key(pair.key.clone()) {
-                env.set(pair.key.deref(), pair.value.clone());
+            if env.map.contains_key(&pair.key) {
+                env.set(&pair.key, pair.value.clone());
             } else {
-                env.create(pair.key.deref(), pair.value.clone());
+                env.create(&pair.key, pair.value.clone());
             }
         }
 
@@ -198,7 +218,7 @@ impl Environment {
 
 impl PartialEq for Environment {
     fn eq(&self, other: &Self) -> bool {
-        other.map.eq(self.map.clone()) && other.func_map.eq(self.func_map.clone())
+        other.map == self.map && other.func_map == self.func_map.clone()
     }
 }
 
