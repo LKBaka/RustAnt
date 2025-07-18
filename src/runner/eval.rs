f3d6f0a86fe05957789366846a6c5f98a7a58b8b
APIBox <LinkerVB@outlook.com>
2025-07-18 18:29:54 +0800

    ⚠️ 现在使用 runner.eval 函数需要提供 Args 结构体。现在支持在求值前打印 AST

--- src/runner/eval.rs
+++ src/runner/eval.rs
@@ -1,10 +1,12 @@
+use crate::arg_structure::arg_structure::Args;
+use crate::ast::ast::Node;
 use crate::environment::environment::Environment;
 use crate::lexer::lexer::Lexer;
 use crate::parser::parser::Parser;
 use crate::object::object::{Object, ERROR};
 use crate::evaluator::evaluator::Evaluator;
 
-pub fn eval(code: String, file: String, env: &mut Environment) -> Option<Object> {
+pub fn eval(code: String, file: String, env: &mut Environment, args: &Args) -> Option<Object> {
     let mut code_lexer: Lexer = Lexer::new(code, file);
     let tokens = code_lexer.get_tokens();
 
@@ -14,21 +16,25 @@ pub fn eval(code: String, file: String, env: &mut Environment) -> Option<Object>
     }
 
     let mut parser: Parser = Parser::new(tokens.clone());
-    let program = parser.parse_program();
+    let mut program = parser.parse_program();
 
     if parser.contains_error() {
         parser.print_errors();
         return None
     }
 
+    if args.print_ast {
+        println!("AST: {}\n", program.to_string());
+    }
+
     let mut evaluator = Evaluator::new();
-    let result = evaluator.eval(program, env);
+    let result = evaluator.eval(&mut program, env);
 
     match result {
         None => {None},
-        Some(result) => {
-            if result.get_type() != ERROR {
-                Some(result)
+        Some(it) => {
+            if it.get_type() != ERROR {
+                Some(it)
             } else {
                 None
             }
