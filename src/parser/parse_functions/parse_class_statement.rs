7331250821f1432089fd47deebaf22a2babb8a26
APIBox <LinkerVB@outlook.com>
2025-07-18 18:20:12 +0800

    🛠️ 修复了先前的 Bug (例如不正确的继承解析, 目前只支持单继承)

--- src/parser/parse_functions/parse_class_statement.rs
+++ src/parser/parse_functions/parse_class_statement.rs
@@ -1,67 +1,85 @@
 use std::any::Any;
 
 use crate::ast::ast::Statement;
-use crate::ast::expressions::identifier::create_identifier;
 use crate::ast::expressions::identifier::Identifier;
 use crate::ast::statements::block_statement::BlockStatement;
 use crate::ast::statements::class_statement::create_class_statement;
 use crate::parser::parser::Parser;
 use crate::token::token_type::TokenType;
-use crate::token::token_type::TokenType::LBrace;
-use crate::token::token_type::TokenType::Ident;
 
 use super::parse_block_statement::parse_block_statement;
 
 pub fn parse_class_statement(parser: &mut Parser) -> Option<Box<dyn Statement>> {
-    let mut base: Option<Identifier> = None;
     let token = parser.cur_token.clone();
+
+    let mut parent_class_ident = None;
 
     parser.next_token(); // 离开 class 词法单元
 
-    // 判断是否为标识符
-    if !parser.expect_cur(Ident) {
+    if !parser.expect_cur(TokenType::Ident) {
         return None;
     }
 
-    let ident = create_identifier(parser.cur_token.clone(), parser.cur_token.value.clone());
+    let class_ident = Some(Identifier {
+        token: parser.cur_token.clone(),
+        value: parser.cur_token.value.clone(),
+    });
 
     parser.next_token(); // 离开标识符
 
     if parser.cur_token_is(TokenType::Colon) {
-        if !parser.expect_peek(Ident) {
+        parser.next_token(); // 离开冒号
+
+        if !parser.expect_cur(TokenType::Ident) {
+            parser.errors.push(
+                format!(
+                    "missing parent class name. at file <{}>, line {}",
+                    parser.cur_token.file, parser.cur_token.line
+                )
+            );
             return None;
         }
 
-        parser.next_token(); // 离开冒号   
-        parser.next_token(); // 离开标识符     
+        // 处理父类标识符
+        parent_class_ident = Some(Identifier {
+            token: parser.cur_token.clone(),
+            value: parser.cur_token.value.clone(),
+        });
 
-        base = Some(create_identifier(parser.cur_token.clone(), parser.cur_token.value.clone()));
+        parser.next_token(); // 离开父类标识符 (正常应前进到左大括号)
     }
 
-    if !parser.expect_cur(LBrace) {
-        return None;
-    }
-
-    parser.next_token(); // 离开大括号
 
     let block = parse_block_statement(parser);
-    if let Option::None = block {
-        parser.errors.push(
-            format!(
-                "missing block. at file <{}>, line {}",
-                parser.cur_token.file, parser.cur_token.line
-            )
-        );
+    
+    match block {
+        Some(block) => {
+            let class_ident = if let Some(ident) = class_ident {
+                ident
+            } else {
+                parser.errors.push(
+                    format!(
+                        "missing class name. at file <{}>, line {}",
+                        parser.cur_token.file, parser.cur_token.line
+                    )
+                );
+                
+                return None;
+            };
 
-        return None;
+            if let Some(block) = (block as Box<dyn Any>)
+                .downcast_ref::<BlockStatement>() 
+            {
+                Some(Box::new(
+                    create_class_statement(
+                        token, 
+                        class_ident, 
+                        parent_class_ident, 
+                        block.clone()
+                    )
+                ))
+            } else { None }
+        },
+        None => None,
     }
-
-    Some(Box::new(
-        create_class_statement(
-            token, 
-            ident,
-            base, 
-            (block.unwrap() as Box<dyn Any>).downcast_ref::<BlockStatement>()?.to_owned()
-        )
-    ))
 }
