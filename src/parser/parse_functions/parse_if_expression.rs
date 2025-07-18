bfbfda17e328fee07ae578d05744b93853b9c988
APIBox <LinkerVB@outlook.com>
2025-07-18 18:24:52 +0800

    🔨 健壮性++ 现在在缺少条件语句，If-Block, Else-Block 时会正常报错了

--- src/parser/parse_functions/parse_if_expression.rs
+++ src/parser/parse_functions/parse_if_expression.rs
@@ -1,65 +1,90 @@
-use crate::ast::ast::Expression;
 use crate::ast::expressions::if_expression::create_if_expression;
 use crate::parser::parse_functions::parse_block_statement::parse_block_statement;
 use crate::parser::parse_functions::parse_else_if_expression::parse_else_if_expression;
-use crate::parser::parser::Parser;
 use crate::parser::precedence::Precedence;
-use crate::token::token_type::TokenType::{Else, Eol, If};
+use crate::token::token_type::TokenType;
+
+use crate::ast::ast::Expression;
+use crate::parser::parser::Parser;
+
 
 pub fn parse_if_expression(parser: &mut Parser) -> Option<Box<dyn Expression>> {
     let token = parser.cur_token.clone();
-    
-    // 解析条件表达式
-    parser.next_token(); // 离开 if 关键字
-    let condition = parser.parse_expression(Precedence::Lowest).unwrap();
 
-    parser.next_token(); // 离开条件语句
+    parser.next_token(); // 离开 if 词法单元
 
-    // 跳过换行符
-    while parser.cur_token_is(Eol) {
-        parser.next_token();
-    }
-    
-    // 解析默认分支
-    let consequence = parse_block_statement(parser);
-    parser.next_token(); // 离开右大括号
+    let condition = match parser.parse_expression(Precedence::Lowest) {
+        Some(expr) => expr,
+        None => {
+            parser.errors.push(
+                format!(
+                    "missing condition. at file <{}>, line {}",
+                    parser.cur_token.file, parser.cur_token.line
+                )
+            );
+            return None;
+        }
+    };
 
-    if let Some(consequence) = consequence {
-        let mut else_if_expressions = Vec::new();
-        let mut alternative = None;
+    parser.next_token(); // 离开表达式 (正常应跳转到左大括号)
 
-        // 处理 else if 和 else 分支
-        while parser.cur_token_is(Else) {
-            parser.next_token(); // 离开 else 关键字
+    let consequence = match parse_block_statement(parser) { 
+        Some(block) => block,
+        None => {
+            parser.errors.push(
+                format!(
+                    "missing if body. at file <{}>, line {}",
+                    parser.cur_token.file, parser.cur_token.line
+                )
+            ); return None;
+        }
+    };
 
-            // 检查是否是 else if
-            if parser.cur_token_is(If) {
-                parser.next_token(); // 离开 if 关键字
-                
-                if let Some(else_if_expr) = parse_else_if_expression(parser) {
-                    else_if_expressions.push(else_if_expr);
-                }
-            } else {
-                // 处理普通的 else 分支
-                // 跳过换行符
-                while parser.cur_token_is(Eol) {
-                    parser.next_token();
-                }
+    let mut else_if_expressions = vec![];
 
-                alternative = parse_block_statement(parser);
-                parser.next_token(); // 离开右大括号
-                break; // 遇到 else 分支后不再继续处理
+    while parser.peek_token_is(TokenType::Else) {
+        parser.next_token(); // 前进到 else 词法单元
+
+        if parser.peek_token_is(TokenType::If) {
+            parser.next_token(); // 前进到 if 词法单元
+
+            let else_if_expression = parse_else_if_expression(parser);
+            if let Some(else_if_expression) = else_if_expression {
+                else_if_expressions.push(else_if_expression); 
             }
+
+            continue;
         }
 
-        Some(Box::new(create_if_expression(
+        // 处理 else
+        parser.next_token(); // 前进到左大括号
+
+        let else_block = match parse_block_statement(parser) { 
+            Some(block) => block,
+            None => {
+                parser.errors.push(
+                    format!(
+                        "missing else body. at file <{}>, line {}",
+                        parser.cur_token.file, parser.cur_token.line
+                    )
+                ); return None;
+            }
+        };
+
+        return Some(Box::new(create_if_expression(
             token,
             condition,
             consequence,
-            alternative,
-            if else_if_expressions.is_empty() { None } else { Some(else_if_expressions) }
-        )))
-    } else {
-        None
+            Some(else_block), 
+            Some(else_if_expressions),
+        )));
     }
-} 
+
+    Some(Box::new(create_if_expression(
+        token,
+        condition,
+        consequence,
+        None, 
+        Some(else_if_expressions),
+    )))
+}
