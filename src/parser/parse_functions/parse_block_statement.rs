a3cacb0a22284ab76df2ff82caee50b374c28657
APIBox <LinkerVB@outlook.com>
2025-07-18 18:18:08 +0800

    ⚠️ 现在有需要离开右括号的情况自行处理

--- src/parser/parse_functions/parse_block_statement.rs
+++ src/parser/parse_functions/parse_block_statement.rs
@@ -1,22 +1,30 @@
 use crate::ast::ast::Statement;
 use crate::ast::statements::block_statement::create_block_statement;
 use crate::parser::parser::Parser;
-use crate::token::token_type::TokenType::{Eof, RBrace};
+use crate::token::token_type::TokenType::{LBrace, RBrace};
 
 pub fn parse_block_statement(parser: &mut Parser) -> Option<Box<dyn Statement>> {
     let token = parser.cur_token.clone();
-    let mut statements = vec![];
+
+    if !parser.expect_cur(LBrace) {
+        return None;
+    }
 
     parser.next_token(); // 离开左括号
 
-    while !parser.cur_token_is(RBrace) && !parser.cur_token_is(Eof) {
-        let statement = parser.parse_statement();
-        if let Some(it) = statement {
-            statements.push(it);
-        }
+    let mut statements = vec![];
 
-        parser.next_token(); // 离开语句
+    while !parser.cur_token_is(RBrace) {
+        let statement = parser.parse_statement();
+
+        statements.push(statement?);
+
+        parser.next_token() // 离开语句
     }
 
-    Some(Box::new(create_block_statement(token, statements)))
+    // WARNING: 有需要离开右括号的情况自行处理
+
+    Some(Box::new(
+        create_block_statement(token, statements)
+    ))
 }
