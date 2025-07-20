c7b1319aab58d55f62580fd1c94c76364a0d6f7d
APIBox <LinkerVB@outlook.com>
2025-07-20 16:57:54 +0800

    🔨 新增了对代码 "Main::run()" 词法单元化的校验测试

--- src/lexer/test.rs
+++ src/lexer/test.rs
@@ -54,3 +54,35 @@ fn test_lexer_2() {
         println!("Got: {:?}", result);
     });
 }
+
+
+#[test]
+fn test_lexer_3() {
+    use super::lexer::Lexer;
+    
+    use crate::token::token_type::TokenType;
+    use crate::token::token::Token;
+    use crate::utils::assert_eq;
+
+    let file: &'static str = "__test_lexer_3__";
+
+
+    let expected_tokens = vec![
+        Token::new(TokenType::Class, "Main".into(), file.into(), 1),
+        Token::new(TokenType::GetClassMember, "::".into(), file.into(), 1),
+        Token::new(TokenType::Ident, "run".into(), file.into(), 1),
+        Token::new(TokenType::LParen, "(".into(), file.into(), 1),
+        Token::new(TokenType::RParen, ")".into(), file.into(), 1),
+    ];
+
+    let code = "Main::run()";
+    let mut lexer = Lexer::new(code.to_string(), file.into());
+    
+    let result = lexer.get_tokens();
+
+    assert_eq(&result, &expected_tokens, || {
+        println!("Expected: {:?}", expected_tokens);
+        println!("Got: {:?}", result);
+    });
+}
+
