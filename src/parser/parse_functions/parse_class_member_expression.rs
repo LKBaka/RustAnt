791edccb29ca03bbf55bf2b0b9c07d23b84dee03
APIBox <LinkerVB@outlook.com>
2025-07-20 17:02:27 +0800

    🥳 新增 ClassMemberExpression (类成员访问表达式) 的解析函数

---
+++ src/parser/parse_functions/parse_class_member_expression.rs
@@ -0,0 +1,27 @@
+use crate::ast::ast::Expression;
+use crate::ast::expressions::class_member_expression::create_class_member_expression;
+use crate::parser::parser::Parser;
+use crate::parser::precedence::Precedence;
+
+
+pub fn parse_class_member_expression(parser: &mut Parser, left: Box<dyn Expression>) -> Option<Box<dyn Expression>> {
+    let token = parser.cur_token.clone();
+    let left_expression = left;
+
+    parser.next_token(); // 离开双冒号 (::)
+
+    let right_expression = parser.parse_expression(Precedence::ObjMember);
+
+    match right_expression {
+        None => {
+            parser.errors.push(
+                format!("missing expression. at file <{}>, line {}", token.file, token.line)
+            ); None
+        }
+        Some(right_expression) => {
+            Some(
+                Box::new(create_class_member_expression(token, left_expression, right_expression))
+            )
+        }
+    }
+}
