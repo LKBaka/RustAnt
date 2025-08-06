fb6eea3a5bc8687d290bc736e1670915cc82e28e
APIBox <LinkerVB@outlook.com>
2025-08-06 11:08:34 +0800

    🥳 新增辅助函数 parse 需要参数 code, file (String), 返回一个 Result<Program, ()> 枚举

---
+++ src/parser/utils.rs
@@ -0,0 +1,23 @@
+use crate::ast::ast::Program;
+
+pub fn parse(code: String, file: String) -> Result<Program, ()> {
+    use crate::{lexer::lexer::Lexer, parser::parser::Parser};
+
+    let mut code_lexer: Lexer = Lexer::new(code, file);
+    let tokens = code_lexer.get_tokens();
+
+    if code_lexer.contains_error() {
+        code_lexer.print_errors();
+        return Err(())
+    }
+
+    let mut parser: Parser = Parser::new(tokens.clone());
+    let program = parser.parse_program();
+
+    if parser.contains_error() {
+        parser.print_errors();
+        return Err(())
+    }
+
+    Ok(program)
+}
