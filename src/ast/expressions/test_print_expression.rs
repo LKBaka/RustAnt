116771074cea3e0c98e5eedccb9111953591c416
APIBox <LinkerVB@outlook.com>
2025-08-13 13:28:59 +0800

    🥳 加入了 TestPrintExpression 表达式以在 print 函数加入之前打印对象

---
+++ src/ast/expressions/test_print_expression.rs
@@ -0,0 +1,39 @@
+use crate::ast::ast::{Expression, Node};
+use crate::token::token::Token;
+
+use crate::impl_node;
+
+impl Clone for TestPrintExpression {
+    fn clone(&self) -> Self {
+        Self {
+            value: self.value.clone(),
+            token: self.token.clone()
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct TestPrintExpression {
+    pub value: Box<dyn Expression>,
+    pub token: Token,
+}
+
+impl Node for TestPrintExpression {
+    fn token_literal(&self) -> String {
+        self.token.value.clone()
+    }
+
+    fn to_string(&self) -> String {
+        format!("TestPrint {}", self.value.to_string())
+    }
+}
+
+impl Expression for TestPrintExpression {}
+
+impl_node!(TestPrintExpression);
+
+pub fn create_test_print_expression(token: Token, value: Box<dyn Expression>) -> TestPrintExpression {
+    TestPrintExpression {
+        token, value
+    }
+}
