37a4859718f0bf5d0e58e112159a20b01b08bc13
APIBox <LinkerVB@outlook.com>
2025-08-13 13:48:08 +0800

    🥳 新增 TestPrintExpression 表达式的语法解析

---
+++ src/parser/parse_functions/parse_test_print_expression.rs
@@ -0,0 +1,22 @@
+use crate::ast::ast::Expression;
+use crate::ast::expressions::test_print_expression::create_test_print_expression;
+use crate::parser::parser::Parser;
+use crate::parser::precedence::Precedence;
+
+pub fn parse_test_print_expression(parser: &mut Parser) -> Option<Box<dyn Expression>> {
+    let token = parser.cur_token.clone();
+
+    parser.next_token(); // 离开 TestPrint 词法单元
+
+    let to_print_expr = if let Some(it) = parser.parse_expression(Precedence::Lowest) {
+        it
+    } else {
+        parser.errors.push(format!(
+            "missing expression to print. at file <{}>, line {}",
+            parser.cur_token.file, parser.cur_token.line
+        ));
+        return None;
+    };
+
+    Some(Box::new(create_test_print_expression(token, to_print_expr)))
+}
