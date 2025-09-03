bf8873ffc9249c2375b42396f1b431950ed438d4
APIBox <LinkerVB@outlook.com>
2025-09-03 13:18:07 +0800

    😎 新增 NoneLiteral 的语法解析

---
+++ src/parser/parse_functions/parse_none.rs
@@ -0,0 +1,9 @@
+use crate::ast::ast::Expression;
+use crate::ast::expressions::none_literal::create_none_literal;
+use crate::parser::parser::Parser;
+
+pub fn parse_none(parser: &mut Parser) -> Option<Box<dyn Expression>> {
+    Some(Box::new(create_none_literal(
+        parser.cur_token.clone(),
+    )))
+}
