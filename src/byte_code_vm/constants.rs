f9692b6edfce8c9e5175f3b4d8fa58fd2fad4583
APIBox <LinkerVB@outlook.com>
2025-09-03 13:01:16 +0800

    🤨 新增常量 NONE_OBJ

--- src/byte_code_vm/constants.rs
+++ src/byte_code_vm/constants.rs
@@ -1,9 +1,10 @@
 use once_cell::sync::Lazy;
 
-use crate::object::{ant_boolean::AntBoolean, ant_uninit::AntUninit};
+use crate::object::{ant_boolean::AntBoolean, ant_none::AntNone, ant_uninit::AntUninit, object::Object};
 
 pub static TRUE: Lazy<AntBoolean> = Lazy::new(|| AntBoolean::from(true));
 pub static FALSE: Lazy<AntBoolean> = Lazy::new(|| AntBoolean::from(false));
 pub static UNINIT_OBJ: Lazy<AntUninit> = Lazy::new(AntUninit::create);
+pub static NONE_OBJ: Lazy<Object> = Lazy::new(|| AntNone::new());
 
 pub const FAKE_OFFSET_JUMP: u16 = 91 * 2 + 78 * 4 + 13 * 2;
