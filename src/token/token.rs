259cddca1529d4bca574bfd05ed095fe00942c21
APIBox <LinkerVB@outlook.com>
2025-09-04 13:30:46 +0800

    🥳 现在每个词法单元都会带上对应的行列号

--- src/token/token.rs
+++ src/token/token.rs
@@ -3,20 +3,22 @@ use std::fmt::Debug;
 use crate::constants::NULL_CHAR;
 use crate::token::token_type::TokenType;
 
-#[derive(Clone, PartialEq, Eq)]
+#[derive(Clone, PartialEq, Eq, Debug)]
 pub struct Token {
     pub token_type: TokenType,
     pub value: String,
     pub file: String,
-    pub line: i64,
+    pub line: usize,
+    pub column: usize,
 }
 
 impl Token {
-    pub fn new(token_type: TokenType, value: String, file: String, line: i64) -> Token {
+    pub fn new(token_type: TokenType, value: String, file: String, line: usize, column: usize) -> Token {
         Token {
             token_type,
             value,
             line,
+            column,
             file,
         }
     }
@@ -28,23 +30,13 @@ impl Token {
         )
     }
 
-    pub fn eof(file: String, line: i64) -> Token {
+    pub fn eof(file: String, line: usize, column: usize) -> Token {
         Token {
             token_type: TokenType::Eof,
             value: NULL_CHAR.to_string(),
             file,
             line,
+            column,
         }
-    }
-}
-
-impl Debug for Token {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        f.debug_struct("Token")
-            .field("type", &self.token_type) // 使用 token_type 的 Debug 实现
-            .field("value", &self.value) // 字符串直接显示内容
-            .field("file", &self.file) // 文件名保留完整路径
-            .field("line", &self.line) // 行号直接显示数值
-            .finish()
     }
 }
