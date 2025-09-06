45b09df353cf269c03ee1a2be022aa92f9db0838
APIBox <LinkerVB@outlook.com>
2025-09-06 16:43:32 +0800

    🟡 新增了 Use 语句的AST

--- src/ast/statements/mod.rs
+++ src/ast/statements/mod.rs
@@ -2,3 +2,4 @@ pub mod block_statement;
 pub mod class_statement;
 pub mod let_statement;
 pub mod while_statement;
+pub mod use_statement;
---
+++ src/ast/statements/use_statement.rs
@@ -0,0 +1,34 @@
+use crate::ast::ast::{Node, Statement};
+use crate::ast::expressions::identifier::Identifier;
+
+use crate::token::token::Token;
+
+#[derive(Debug, Clone)]
+pub struct UseStatement {
+    pub token: Token,
+    pub name: Identifier,
+    pub alias: Option<Identifier>,
+}
+
+impl Node for UseStatement {
+    fn token_literal(&self) -> String {
+        self.token.value.clone()
+    }
+
+    fn to_string(&self) -> String {
+        match &self.alias {
+            Some(alias) => format!("use {} as {}", self.name.to_string(), alias.to_string()),
+            None => format!("use {}", self.name.to_string())
+        }
+    }
+}
+
+impl Statement for UseStatement {}
+
+pub fn create_use_statement(
+    token: Token,
+    name: Identifier,
+    alias: Option<Identifier>,
+) -> UseStatement {
+    UseStatement { token, name, alias }
+}
