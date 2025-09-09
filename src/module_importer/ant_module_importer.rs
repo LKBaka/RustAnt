d06691ea9ed828b93ad4febd81d7bef5cb7343c6
APIBox <LinkerVB@outlook.com>
2025-09-09 12:43:27 +0800

    🥳 ModuleImporter 回归

--- src/module_importer/ant_module_importer.rs
+++ src/module_importer/ant_module_importer.rs
@@ -1,9 +1,71 @@
+use std::fs;
+
+use hashbrown::HashMap;
+
+use crate::{builtin::builtin_map::BUILTIN_MAP_INDEX, byte_code_vm::{compiler::{compiler::Compiler, symbol_table::symbol_table::Symbol}, vm::vm::Vm}, obj_enum::object::Object, object::{ant_class::AntClass, ant_string::AntString, object::{IAntObject, UNINIT}}, parser::utils::parse};
+
 pub struct AntModuleImporter {
-    file: String
+    pub file: String
 }
 
 impl AntModuleImporter {
-    pub fn import() -> () {
-        
+    pub fn import(&self) -> Result<AntClass, String> {
+        let code = fs::read_to_string(&self.file);
+        if let Err(err) = code {
+            return Err(format!("{err}"))
+        }
+
+        let program = parse(code.unwrap(), self.file.clone());
+
+        if let Ok(it) = program {
+            #[cfg(feature = "debug")]
+            {
+                use colored::Colorize;
+
+                use crate::ast::ast::Node;
+
+                println!("AST: {}", it.to_string().yellow());
+            }
+
+            let mut compiler = Compiler::new();
+
+            let result = compiler.start_compile(it);
+
+            let bytecode =  match result {
+                Ok(_) => compiler.bytecode(),
+                Err(msg) => return Err(format!("error compile module: {msg}")),
+            };
+
+            let mut vm = Vm::new(bytecode);
+            match vm.run() {
+                Ok(_) => {},
+                Err(msg) => return Err(format!("error running module: {msg}"))   
+            };
+
+            let vm_globals = vm.globals.borrow();
+            let symbol_table = compiler.symbol_table.borrow();
+
+            let mut globals = HashMap::new();
+
+            symbol_table
+                .store
+                .iter()
+                .for_each(|(name, symbol)| {
+                    if BUILTIN_MAP_INDEX.contains(name) {
+                        return;
+                    }
+
+                    let global = vm_globals[symbol.index].borrow().clone();
+
+                    globals.insert(
+                        Object::AntString(AntString::new(name.to_string())),
+                        global
+                    );
+                });
+
+            return Ok(AntClass::from(globals))
+        }
+
+        Err(String::from("parse failed!"))
     }
 }
--- src/module_importer/importer_enum.rs
+++ src/module_importer/importer_enum.rs
@@ -1,12 +1,54 @@
-use crate::module_importer::ant_module_importer::AntModuleImporter;
+use std::{path::{Path, PathBuf}, str::FromStr};
 
-pub enum Importer {
-    AntModuleImporter(AntModuleImporter),
-    NativeModuleImporter(),
+use crate::{constants::MODULE_PATHS, module_importer::ant_module_importer::AntModuleImporter, obj_enum::object::Object, object::ant_class::AntClass};
+
+pub struct ModuleImporter;
+
+impl ModuleImporter {
+    pub fn import(imports: Vec<&str>) -> Vec<Result<AntClass, String>> {
+        let mut results = vec![];
+
+        let mut module_paths: Vec<String> = vec![];
+
+        for module_path in &MODULE_PATHS
+            .lock()
+            .unwrap()
+            .items 
+        {
+            if let Object::AntString(s) = module_path {
+                module_paths.push(s.value.clone());
+            }
+        }
+
+        for module_path in module_paths  {
+            for import in &imports {
+                let path_buf = PathBuf::from_str(&module_path)
+                    .expect(&format!("invaild path: {module_path}"));
+
+                let mut try_ant_mod = path_buf.clone();
+                try_ant_mod.push(format!("{import}.ant"));
+
+                // let mut try_native_mod = path_buf.clone();
+                // try_native_mod.push(format!("{import}.dll"));
+
+                if try_ant_mod.exists() {
+                    results.push(Self::import_ant_module(&try_ant_mod));
+                    continue;
+                }
+            }
+        }
+
+        results
+    }
+
+    fn import_ant_module(file: &Path) -> Result<AntClass, String> {
+        let ant_mod_importer = AntModuleImporter {
+            file: file
+                .to_str()
+                .unwrap()
+                .to_string()
+        };
+
+        ant_mod_importer.import()
+    }
 }
-
-pub struct ModuleImporter {
-    pub imports: Vec<String>,
-    pub importer: Importer,
-}
-
