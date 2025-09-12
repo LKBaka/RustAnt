e3fb2cac9e596a3a07f2c67b013b0e9fcdc274ad
APIBox <LinkerVB@outlook.com>
2025-09-12 21:33:46 +0800

    🥳 新增 Result 数据类型 (不完全)

--- src/builtin/builtin_map.rs
+++ src/builtin/builtin_map.rs
@@ -1,59 +1,111 @@
+use std::collections::HashMap;
+
 use lazy_static::lazy_static;
 
-use crate::{builtin::builtin_func::{builtin_clear, builtin_copy, builtin_force_exit, builtin_id, builtin_len, builtin_now, builtin_obj_info, builtin_print, builtin_shell}, object::ant_native_function::{create_ant_native_function, AntNativeFunction}};
+use crate::{
+    builtin::builtin_func::{
+        builtin_clear, builtin_copy, builtin_force_exit, builtin_id, builtin_len, builtin_now,
+        builtin_obj_info, builtin_print, builtin_shell,
+    },
+    byte_code_vm::constants::NONE_OBJ,
+    obj_enum::object::Object,
+    object::{
+        ant_class::AntClass, ant_native_function::create_ant_native_function, object::IAntObject,
+    },
+};
 
 lazy_static! {
-    pub static ref BUILTIN_MAP: hashbrown::HashMap<String, AntNativeFunction> = {
+    pub static ref BUILTIN_MAP: hashbrown::HashMap<String, Object> = {
         let mut m = hashbrown::HashMap::new();
 
         m.insert(
             "print".into(),
-            create_ant_native_function(None, builtin_print)
+            Object::AntNativeFunction(create_ant_native_function(None, builtin_print)),
         );
 
         m.insert(
             "len".into(),
-            create_ant_native_function(None, builtin_len)
+            Object::AntNativeFunction(create_ant_native_function(None, builtin_len)),
         );
 
         m.insert(
             "copy".into(),
-            create_ant_native_function(None, builtin_copy)
+            Object::AntNativeFunction(create_ant_native_function(None, builtin_copy)),
         );
 
         m.insert(
             "now".into(),
-            create_ant_native_function(None, builtin_now)
+            Object::AntNativeFunction(create_ant_native_function(None, builtin_now)),
         );
 
         m.insert(
             "id".into(),
-            create_ant_native_function(None, builtin_id)
+            Object::AntNativeFunction(create_ant_native_function(None, builtin_id)),
         );
 
         m.insert(
             "obj_info".into(),
-            create_ant_native_function(None, builtin_obj_info)
+            Object::AntNativeFunction(create_ant_native_function(None, builtin_obj_info)),
         );
-        
+
         m.insert(
             "shell".into(),
-            create_ant_native_function(None, builtin_shell)
+            Object::AntNativeFunction(create_ant_native_function(None, builtin_shell)),
         );
-        
+
         m.insert(
             "clear".into(),
-            create_ant_native_function(None, builtin_clear)
+            Object::AntNativeFunction(create_ant_native_function(None, builtin_clear)),
         );
 
         m.insert(
             "force_exit".into(),
-            create_ant_native_function(None, builtin_force_exit)
+            Object::AntNativeFunction(create_ant_native_function(None, builtin_force_exit)),
+        );
+
+        m.insert(
+            "Result".into(),
+            Object::AntClass(AntClass::from({
+                let native_func = |args: Vec<std::rc::Rc<std::cell::RefCell<Object>>>| {
+                    let o = args[0].borrow();
+
+                    let me = match &*o {
+                        Object::AntClass(clazz) => clazz,
+                        _ => return Err(format!("expected an class (self) got {}", o.inspect())),
+                    };
+
+                    let err = match me.map.get("err") {
+                        Some(it) => it,
+                        None => return Err(format!("object '{}' has no field 'err'", me.inspect()))
+                    };
+
+                    if &*err == &*NONE_OBJ {
+                        let value = me.map.get("value").ok_or_else(|| {
+                            format!("object '{}' has no field 'value'", me.inspect(),)
+                        })?;
+
+                        return Ok(Some(value.clone()));
+                    }
+
+                    Err(format!("unwrap failed: {}", err.inspect()))
+                };
+
+                let mut m = HashMap::new();
+
+                m.insert("value".into(), NONE_OBJ.clone());
+                m.insert("err".into(), NONE_OBJ.clone());
+
+                m.insert(
+                    "unwrap".into(),
+                    Object::AntNativeFunction(create_ant_native_function(None, native_func)),
+                );
+
+                m
+            })),
         );
 
         m
     };
-
     pub static ref BUILTIN_MAP_INDEX: Vec<String> = {
         vec![
             "print".into(),
@@ -65,6 +117,7 @@ lazy_static! {
             "shell".into(),
             "clear".into(),
             "force_exit".into(),
+            "Result".into(),
         ]
     };
-}   
+}
