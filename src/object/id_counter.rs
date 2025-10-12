81af56bb203d0aa3892accf2bcad5204a62b58f7
APIBox <LinkerVB@outlook.com>
2025-10-12 18:55:17 +0800

    ⚠️⚠️♿ 性能提升 以线程不安全的代价换取获取id时的速度增加

--- src/object/id_counter.rs
+++ src/object/id_counter.rs
@@ -1,8 +1,19 @@
-use std::sync::atomic::{AtomicUsize, Ordering};
+// use std::sync::atomic::{AtomicUsize, Ordering};
 
-static COUNTER: AtomicUsize = AtomicUsize::new(0);
+// static COUNTER: AtomicUsize = AtomicUsize::new(0);
 
-#[inline]
+// #[inline]
+// pub fn next_id() -> usize {
+//     COUNTER.fetch_add(1, Ordering::Relaxed)
+// }
+
+static mut COUNTER: usize = 0;
+
+#[inline(always)]
 pub fn next_id() -> usize {
-    COUNTER.fetch_add(1, Ordering::Relaxed)
+    let cur = unsafe { COUNTER };
+
+    unsafe { COUNTER += 1 }
+
+    cur
 }
